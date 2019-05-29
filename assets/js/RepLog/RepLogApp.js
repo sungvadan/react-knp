518b61793c74b7e18f34d00c5bbdd790991f09df
Van Truong PHAN <svd.phan@gmail.com>
2019-05-29 20:01:01 +0200

    state

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react'
 
 export default class RepLogApp extends Component
 {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      highlightedRowId: null,
+    }
+  }
   render() {
+    const { highlightedRowId } = this.state;
+
     let heart = '';
     if (this.props.withHeart) {
       heart = <span>Love</span>;
@@ -30,7 +39,10 @@ export default class RepLogApp extends Component
           </thead>
           <tbody>
           {repLogs.map((repLog) => (
-            <tr key={repLog.id}>
+            <tr
+              key={repLog.id}
+              className={highlightedRowId === repLog.id ? 'info' : ''}
+            >
               <td>{repLog.itemLabel}</td>
               <td>{repLog.reps}</td>
               <td>{repLog.totalWeightLifted}</td>
@@ -47,6 +59,37 @@ export default class RepLogApp extends Component
           </tr>
           </tfoot>
         </table>
+
+        <form className="form-inline" noValidate>
+          <div className="form-group">
+            <label className="sr-only control-label required" htmlFor="rep_log_item">
+              What did you lift?
+            </label>
+            <select id="rep_log_item"
+                    name="item"
+                    required="required"
+                    className="form-control">
+              <option value="">What did you lift?</option>
+              <option value="cat">Cat</option>
+              <option value="fat_cat">Big Fat Cat</option>
+              <option value="laptop">My Laptop</option>
+              <option value="coffee_cup">Coffee Cup</option>
+            </select>
+          </div>
+          { ' ' }
+          <div className="form-group">
+            <label className="sr-only control-label required" htmlFor="rep_log_reps">
+              How many times?
+            </label>
+            <input type="number" id="rep_log_reps"
+                   name="reps" required="required"
+                   placeholder="How many times?"
+                   className="form-control"/>
+          </div>
+          { ' ' }
+          <button type="submit" className="btn btn-primary">I Lifted it!</button>
+        </form>
+
       </div>
     )
   }
