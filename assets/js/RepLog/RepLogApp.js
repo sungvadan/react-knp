6894fa371da484a2ea3a0ab9a66d52ba5d14c969
Van Truong PHAN <svd.phan@gmail.com>
2019-05-29 19:16:12 +0200

    props

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react'
 export default class RepLogApp extends Component
 {
   render() {
-    return <h2>Lift stuff <span>Love</span></h2>
+    let heart = '';
+    if (this.props.withHeart) {
+      heart = <span>Love</span>;
+    }
+    return (
+        <h2>Lift stuff {heart}</h2>
+      )
   }
 }
