609bc6f384a2705335f0716a5a7b3cd76d7cfe07
Van Truong PHAN <svd.phan@gmail.com>
2019-05-29 16:50:49 +0200

    react elements

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,1 +1,11 @@
-console.log('here');
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const el = React.createElement(
+  'h2',
+  null,
+  'Lift History',
+  React.createElement('span', null, ' Love')
+);
+ReactDom.render(el, document.getElementById('lift-stuff-app'));
+console.log(el);
