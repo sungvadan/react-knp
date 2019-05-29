43c631c63574c7379cdb1407d12ed93f82d221bf
Van Truong PHAN <svd.phan@gmail.com>
2019-05-29 17:23:22 +0200

    jsx

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const el = React.createElement(
-  'h2',
-  null,
-  'Lift History',
-  React.createElement('span', null, ' Love')
-);
+const el = <h2>Lift stuff <span>Love</span></h2>
 ReactDom.render(el, document.getElementById('lift-stuff-app'));
 console.log(el);
