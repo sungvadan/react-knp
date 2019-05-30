de2e3323fa23b5018f06448b1d8eafbe08ffc853
Van Truong PHAN <svd.phan@gmail.com>
2019-05-30 12:47:55 +0200

    containner and presentation componnent

--- assets/js/RepLog/RepLogList.js
+++ assets/js/RepLog/RepLogList.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class RepLogList extends Component{
-  render() {
+export default function RepLogList(props){
+  const  repLogs = [
+    { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 120 },
+    { id: 2, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 120 },
+    { id: 3, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 120 },
+  ];
+  const {highlightedRowId, onRowClick} = props;
 
-    const  repLogs = [
-      { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 120 },
-      { id: 2, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 120 },
-      { id: 3, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 120 },
-    ];
-    const {highlightedRowId, onRowClick} = this.props;
-
-    return (
-      <tbody>
-      {repLogs.map((repLog) => (
-        <tr
-          key={repLog.id}
-          className={highlightedRowId === repLog.id ? 'info' : ''}
-          onClick={()=>onRowClick(repLog.id)}
-        >
-          <td>{repLog.itemLabel}</td>
-          <td>{repLog.reps}</td>
-          <td>{repLog.totalWeightLifted}</td>
-          <td>...</td>
-        </tr>
-      ))}
-      </tbody>
-    )
-  }
-
+  return (
+    <tbody>
+    {repLogs.map((repLog) => (
+      <tr
+        key={repLog.id}
+        className={highlightedRowId === repLog.id ? 'info' : ''}
+        onClick={()=>onRowClick(repLog.id)}
+      >
+        <td>{repLog.itemLabel}</td>
+        <td>{repLog.reps}</td>
+        <td>{repLog.totalWeightLifted}</td>
+        <td>...</td>
+      </tr>
+    ))}
+    </tbody>
+  )
 }
