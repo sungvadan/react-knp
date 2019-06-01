b5921222c2b39b6d9fb418a1e8b489b6f0b60215
Van Truong PHAN <svd.phan@gmail.com>
2019-06-01 12:56:36 +0200

    to work directly with dom element

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import uuid from 'uuid/v4'
 import RepLogs from './RepLogs'
 
 export default class RepLogApp extends Component
@@ -10,17 +11,32 @@ export default class RepLogApp extends Component
     this.state = {
       highlightedRowId: null,
       repLogs: [
-        { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 120 },
-        { id: 2, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 120 },
-        { id: 3, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 120 },
+        { id: uuid(), reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 120 },
+        { id: uuid(), reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 120 },
+        { id: uuid(), reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 120 },
       ]
     };
 
     this.handleRowClick = this.handleRowClick.bind(this);
+    this.handleNewItemSubmit = this.handleNewItemSubmit.bind(this);
   }
 
   handleRowClick(repLogId) {
     this.setState({highlightedRowId: repLogId});
+  }
+
+  handleNewItemSubmit(itemLabel, reps) {
+
+    const repLogs = this.state.repLogs;
+    const newReps = {
+      id: uuid(),
+      reps: reps,
+      itemLabel: itemLabel,
+      totalWeightLifted: 156
+    }
+    repLogs.push(newReps);
+    this.setState({repLogs: repLogs })
+
   }
 
   render() {
@@ -29,6 +45,7 @@ export default class RepLogApp extends Component
         {...this.props}
         {...this.state}
         onRowClick={this.handleRowClick}
+        onNewItemSubmit={this.handleNewItemSubmit}
       />
     )
 
--- assets/js/RepLog/RepLogs.js
+++ assets/js/RepLog/RepLogs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import RepLogList from "./RepLogList";
+import RepLogCreator from './RepLogCreator';
 
 function calculTotalWeightLifted(repLogs) {
   let totalWeight = 0;
@@ -16,7 +17,7 @@ const calculateTotalWeightLifted = repLogs => repLogs.reduce((total, repLog) => 
 
 export default function RepLogs(props){
 
-  const { withHeart, highlightedRowId, repLogs, onRowClick } = props;
+  const { withHeart, highlightedRowId, repLogs, onRowClick, onNewItemSubmit } = props;
 
   let heart = '';
   if (withHeart) {
@@ -52,36 +53,7 @@ export default function RepLogs(props){
         </tr>
         </tfoot>
       </table>
-
-      <form className="form-inline" noValidate>
-        <div className="form-group">
-          <label className="sr-only control-label required" htmlFor="rep_log_item">
-            What did you lift?
-          </label>
-          <select id="rep_log_item"
-                  name="item"
-                  required="required"
-                  className="form-control">
-            <option value="">What did you lift?</option>
-            <option value="cat">Cat</option>
-            <option value="fat_cat">Big Fat Cat</option>
-            <option value="laptop">My Laptop</option>
-            <option value="coffee_cup">Coffee Cup</option>
-          </select>
-        </div>
-        { ' ' }
-        <div className="form-group">
-          <label className="sr-only control-label required" htmlFor="rep_log_reps">
-            How many times?
-          </label>
-          <input type="number" id="rep_log_reps"
-                 name="reps" required="required"
-                 placeholder="How many times?"
-                 className="form-control"/>
-        </div>
-        { ' ' }
-        <button type="submit" className="btn btn-primary">I Lifted it!</button>
-      </form>
+      <RepLogCreator onNewItemSubmit={onNewItemSubmit}/>
 
     </div>
   )
@@ -91,5 +63,6 @@ RepLogs.propTypes = {
   withHeart: PropTypes.bool,
   highlightedRowId: PropTypes.any,
   repLogs: PropTypes.array,
-  onRowClick: PropTypes.func.isRequired
+  onRowClick: PropTypes.func.isRequired,
+  onNewItemSubmit: PropTypes.func.isRequired
 }
